4b192428a0e41f93775bad119ebbca602e316f1d
Ishan <koradiaishan335@gmail.com>
2023-03-28 11:36:54 +0530

    remove phone number validation

--- tap_lms/tap_lms/doctype/student/student.js
+++ tap_lms/tap_lms/doctype/student/student.js
@@ -1,10 +1,2 @@
 // Copyright (c) 2023, Techt4dev and contributors
 // For license information, please see license.txt
-
-frappe.ui.form.on("Student", {
-    validate: function (frm) {
-        if (!frm.doc.phone.match(/^\d{10}$/)) {
-            frappe.throw(__("Please make sure phone number has 10 digits."));
-        }
-    },
-});
