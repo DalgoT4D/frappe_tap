0fdae6c7493eb3be9725997c176c7a7fb9f4947e
Ishan <koradiaishan335@gmail.com>
2023-04-12 18:07:28 +0530

    minor change

--- tap_lms/tap_lms/doctype/batch/batch.py
+++ tap_lms/tap_lms/doctype/batch/batch.py
@@ -13,6 +13,6 @@ class Batch(Document):
 			title += self.name1
 
 		if self.start_date:
-			title += f"({datetime.strptime(self.start_date, '%Y-%m-%d').strftime('%b %y')})"
+			title += f" ({datetime.strptime(self.start_date, '%Y-%m-%d').strftime('%b %y')})"
 
 		self.title = title
