0127709c602f1d565f570779b8d6e8f00e04d2e2
Ishan <koradiaishan335@gmail.com>
2023-09-20 12:37:54 +0530

    creating a student in custom api

--- tap_lms/tap_lms/doctype/student/student.py
+++ tap_lms/tap_lms/doctype/student/student.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 from frappe.model.document import Document
 
 class Student(Document):
@@ -10,4 +11,11 @@ class Student(Document):
 
 @frappe.whitelist()
 def register_student():
+	payload = json.loads(frappe.request.data)
+	doc = frappe.new_doc('Student')
+	doc.name1 = payload.get('name1')
+	doc.phone = payload.get('phone')
+	doc.section = payload.get('section')
+	doc.grade = payload.get('grade')
+	doc.insert()
 	return {'status_code':200,  'message': 'Student registered succesfully'}
