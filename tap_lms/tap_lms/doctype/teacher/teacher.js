6b07cd233c79edf6563759ae20a51c06bf4c8011
root <root@instance-frappe.asia-south1-a.c.central-phalanx-297915.internal>
2024-06-28 09:04:29 +0000

    Update school.json and add teacher doctype

---
+++ tap_lms/tap_lms/doctype/teacher/teacher.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Techt4dev and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Teacher', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ tap_lms/tap_lms/doctype/teacher/test_teacher.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Techt4dev and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTeacher(FrappeTestCase):
+	pass
