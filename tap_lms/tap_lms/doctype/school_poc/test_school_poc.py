e271e32695fb2209f2cff5c5ecea86acc6ef9bb2
Ubuntu <ubuntu@ip-172-31-46-169.ap-south-1.compute.internal>
2024-07-02 09:35:58 +0000

    Add changes to school and teacher doctypes and add school_poc doctype

---
+++ tap_lms/tap_lms/doctype/school_poc/test_school_poc.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Techt4dev and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSchool_POC(FrappeTestCase):
+	pass
